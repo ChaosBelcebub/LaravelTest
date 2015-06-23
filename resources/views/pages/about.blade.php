5fadaea1bf0c08a0e70570b9d2710ef7097b7a0a
ChaosBelcebub <mich3ael@web.de>
2015-06-23 17:23:51 +0200

    first try on laravel

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <title>About</title>
+
+</head>
+
+<body>
+
+    <h1>About Me: {!! $first !!} {!! $last !!}</h1>
+
+    <p>Lorem Ipsum</p>
+
+</body>
+
+</html>
